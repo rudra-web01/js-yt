15f1c152bb29e7c45c8ef3646edd69fac090600a
Prince <prince79003@gmail.com>
2025-12-24 15:36:19 +0000

    studied comparisons in js

---
+++ .devcontainer/01_basics/comparisons.js
@@ -0,0 +1,11 @@
+console.log("2">3); //string converted to number.
+console.log(null>0); //false
+console.log(null==0); //false
+console.log(null>=0); //true
+console.log(undefined == 0); //false
+
+//===
+console.log("2"===2); //false
+
+
+
