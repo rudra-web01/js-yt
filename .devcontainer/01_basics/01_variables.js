bbc8fd576c704de3b9d94c716fe5dfb48dd0cd74
Prince <prince79003@gmail.com>
2025-12-24 04:51:00 +0000

    JS basics: variables practice

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const acc_ID= 14453
+let accEmail= "abc@google.com"
+var accPass= "1546"
+accCity="delhi"
+
+//acc_ID= 2 // you cannot change const
+// baki chize change ho sakti hai
+accEmail="rudra@gmail.com"
+accPass="519678"
+accCity="Ghaziabad"
+let accState
+
+console.table([acc_ID, accEmail, accPass, accCity, accState])
+/*
+do not use var always use let 
+*/
