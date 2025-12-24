76688b604caf31ebeb4d2ee7700956fc1be48281
Prince <prince79003@gmail.com>
2025-12-24 10:29:45 +0000

    conversions operations in js

---
+++ .devcontainer/01_basics/02_ConversionOperation.js
@@ -0,0 +1,45 @@
+/*let score= "33"
+
+
+console.log(typeof score);
+console.log(typeof(score));
+//type of score is currently string
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber); */
+// type changed from string to number
+//"33" => 33
+// "33abc" => NaN but still show the number type
+// true => show 1 when we print the variable
+
+/*let isLoggedIn = 1
+
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 => true
+//0 => false
+//"" => false
+//"value" => true
+
+let someNumber = 33
+let stringNumber= String(someNumber)
+console.log(typeof stringNumber);
+
+// **************OPERATIONS****************************
+let value=4
+let negValue=-value
+console.log(negValue);
+
+let str1="prince"
+let str2=" mishra"
+
+let str3= str1+str2
+console.log(str3);
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+3); //first letter is string so whole expression is string
+console.log(1+2+"3"); //first letter is number so whole expression is number
+//console.log(2+"3"+8); => 238
+?*/
+
