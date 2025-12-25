14d2548c73966009c6f131c71aefc0279e9744dc
Prince <prince79003@gmail.com>
2025-12-25 09:35:08 +0000

    learned about different date formats in js

---
+++ .devcontainer/01_basics/05_datesInjs.js
@@ -0,0 +1,15 @@
+/*let myDate= new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(typeof myDate);
+*/
+//let MyCreatedDate= new Date(2022,1,23)
+// let MyCreatedDate= new Date(2022,1,23,5,7)
+let myCreatedDate= new Date("01-14-2022")
+// console.log(MyCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
+
+
