c7eed0fdd08e767823cda1327b4197dee95a4001
Prince <prince79003@gmail.com>
2025-12-27 19:42:27 +0000

    learned about the array basics in js with some functions

---
+++ .devcontainer/01_basics/arrays.js
@@ -0,0 +1,30 @@
+const myArr = [0,1,2,3,4,] 
+const myHeroes= ["spiderman","batman", "superman","thor"]
+// console.log(myArr[2]);
+
+// methods
+// myArr.push(6)
+// myArr.pop()
+// console.log(myArr);
+// console.log(myArr.includes(4));
+// const newArr= myArr.join()
+// console.log(myArr);
+// console.log(typeof newArr);
+// console.log("V ", myArr);
+
+// const myn1 = myArr.slice(1,3)
+// console.log(myn1);
+// console.log("P", myArr);
+
+// const myn2= myArr.splice(1,3)
+// console.log("C", myArr);
+
+// console.log(myn2);
+
+
+
+
+
+
+
+
