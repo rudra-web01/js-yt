ace3a8b69569e5a6e3defb77cb78468f6df78732
Prince <prince79003@gmail.com>
2025-12-30 05:13:40 +0000

    dived deep into some more functions and methods of array

---
+++ .devcontainer/01_basics/02_array.js
@@ -0,0 +1,29 @@
+const marvel_heroes=["thor", "ironman", "spiderman"]
+const dc= ["superman", "flash", "batman"]
+
+// marvel_heroes.push(dc)
+
+// console.log(marvel_heroes);
+// console.log(marvel_heroes [3][1]);
+
+// const allHeroes=marvel_heroes.concat(dc)
+// console.log(allHeroes);
+
+//spread operator(use to join or add two arrays)
+
+// const allHeroes= [...marvel_heroes, ...dc]
+// console.log(allHeroes);
+
+// const an_array=[1,2,3,[4,5,6],8,[3,6,4,[4,9,2]]]
+// const usable_array = an_array.flat(Infinity)
+// console.log(usable_array);
+
+// console.log(Array.isArray("Prince"));
+// console.log(Array.from("Prince"));
+
+// console.log(Array.from({name : "Prince"})); //returns an empty array
+// let score1= 90
+// let score2= 120
+// let score3= 150
+// console.log(Array.of(score1,score2,score3));
+ 
