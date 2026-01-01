c56227530bf895030ecdab43441fc09c07773abe
Prince <prince79003@gmail.com>
2026-01-01 06:48:32 +0000

    learned about some json api

---
+++ .devcontainer/01_basics/objects_03.js
@@ -0,0 +1,32 @@
+const course= {
+    courseName: "js in hindi",
+    prince: "899",
+    courseInstructor: "Hitesh sir"
+}
+
+// course.courseInstructor
+
+//destructuring
+const {courseInstructor : Instructor} = course
+
+//console.log(courseInstructor);
+console.log(Instructor);
+
+//json api
+// {
+//     "name": "prince", 
+//     "courseName": "js in hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
+
+
+
+
+
+
