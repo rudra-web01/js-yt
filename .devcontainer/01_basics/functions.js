77a98ed0a1e82fc9d0d2105f0e1629dada052fd8
Prince <prince79003@gmail.com>
2026-01-06 10:54:33 +0000

    diving deep into functions in js

--- .devcontainer/01_basics/functions.js
+++ .devcontainer/01_basics/functions.js
@@ -38,17 +38,42 @@
 
 //alternate method
 
-function loginUserMessage(username){
-    if(!username) {
-        console.log("Please enter a username");
-        return
+// function loginUserMessage(username){
+//     if(!username) {
+//         console.log("Please enter a username");
+//         return
         
-    }
-    return `${username} just logged in`
-}
+//     }
+//     return `${username} just logged in`
+// }
 
 //console.log(loginUserMessage());
-console.log(loginUserMessage("Pince"));
+// console.log(loginUserMessage("Pince"));
 
+function calculateCartPrice(...num1){
+    return num1
+} ///this is called rest operator(...) when their are unknown number of arguments then we use this. it return the output un the form of array.
+// console.log(calculateCartPrice(2,3,4,6));
+
+const user={
+    username: "prince",
+    price: 299
+}
+function handleObject(anyObject){
+    console.log(`username is ${anyObject.username} and price is ${anyObject.price}`);
+}
+// handleObject(user)
+handleObject({
+    username: "Rudra",
+    price: 477
+})
+ const nArr= [100,40,74,]
+ function returnSecondValue(getArray) {
+    return getArray[1]
+ }
+//  console.log(returnSecondValue(nArr));
+console.log(returnSecondValue([100,40,74]));
+
+ 
 
 
